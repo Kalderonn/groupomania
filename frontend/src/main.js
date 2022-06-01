d3a4e90e062eb8b3b5b8319589a91c82b8385beb
Kalderonn <mikael.evan@hotmail.fr>
2022-06-01 20:31:57 +0200

    build(frontend): add sass and custom.scss

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// // Import Bootstrap and BootstrapVue CSS files (order is important)
+import './assets/custom.scss'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 Vue.config.productionTip = false
 
 new Vue({
